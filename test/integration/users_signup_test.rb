6d3e36c8e240df183611d01c295918417bceab8c
oryzae24 <134453358+oryzae24@users.noreply.github.com>
2026-05-21 16:24:31 +0000

    Finish user signup

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -16,4 +16,14 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     assert_select 'div.alert.alert-danger', /The form contains/ 
     assert_select 'div#error_explanation ul li', minimum: 1
   end
+  test "valid signup information" do
+    assert_difference 'User.count', 1 do
+      post users_path, params: { user: { name:  "Example User",
+                                         email: "user@example.com",
+                                         password:              "password",
+                                         password_confirmation: "password" } }
+    end
+    follow_redirect!
+    assert_template 'users/show'
+  end
 end
