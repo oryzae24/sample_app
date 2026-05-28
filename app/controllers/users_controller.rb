1c956656fa7b64fdeb986e0f9acabccd9dcd0a18
oryzae24 <134453358+oryzae24@users.noreply.github.com>
2026-05-28 20:11:36 +0000

    Add user index with pagination and enhance user management features

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,13 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:edit, :update]
+  before_action :logged_in_user, only: [:index, :edit, :update]
   before_action :correct_user,   only: [:edit, :update]
+
+  def index
+  end
+
+  def index
+    @users = User.paginate(page: params[:page])
+  end
 
   def show
     @user = User.find(params[:id])
