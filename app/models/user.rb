596320660ec10c27b8e4e2bbbbbc555a7a712d8f
oryzae24 <134453358+oryzae24@users.noreply.github.com>
2026-06-04 14:20:23 +0000

    Enhance user model with activation token and digest; update seeds and fixtures for user activation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ApplicationRecord
   has_many :microposts, dependent: :destroy
-  attr_accessor :remember_token
-  before_save { self.email = email.downcase }
+  attr_accessor :remember_token, :activation_token
+  before_save   :downcase_email
+  before_create :create_activation_digest
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@(?!.*\.{2,})[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }, 
@@ -49,6 +50,19 @@ class User < ApplicationRecord
   # 完全な実装は次章の「ユーザーをフォローする」を参照
   def feed
     Micropost.where("user_id = ?", id)
+  end
+
+  private
+
+  # メールアドレスをすべて小文字にする
+ def downcase_email
+  email.downcase!
+end
+
+  # 有効化トークンとダイジェストを作成および代入する
+  def create_activation_digest
+    self.activation_token  = User.new_token
+    self.activation_digest = User.digest(activation_token)
   end
 
 end
