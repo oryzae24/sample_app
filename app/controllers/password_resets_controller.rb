f06531d2848918e44e67621aadeedaf7b182252f
oryzae24 <134453358+oryzae24@users.noreply.github.com>
2026-06-04 18:32:49 +0000

    Add password reset expiration check and handle empty password in update action

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -1,6 +1,8 @@
 class PasswordResetsController < ApplicationController
-  before_action :get_user,   only: [:edit, :update]
-  before_action :valid_user, only: [:edit, :update]
+  before_action :get_user,         only: [:edit, :update]
+  before_action :valid_user,       only: [:edit, :update]
+  before_action :check_expiration, only: [:edit, :update]    # （1）への対応
+
   def new
   end
 
@@ -20,7 +22,29 @@ class PasswordResetsController < ApplicationController
   def edit
   end
 
+  def update
+    if params[:user][:password].empty?                  # （3）への対応
+      @user.errors.add(:password, "can't be empty")
+      render 'edit', status: :unprocessable_entity
+    elsif @user.update(user_params)                     # （4）への対応
+      @user.forget
+      reset_session
+      log_in @user
+      flash[:success] = "Password has been reset."
+      redirect_to @user
+    else
+      render 'edit', status: :unprocessable_entity      # （2）への対応
+    end
+  end
+
   private
+
+    def user_params
+      params.expect(user: [:password, :password_confirmation])
+    end
+
+    # beforeフィルタ
+
     def get_user
       @user = User.find_by(email: params[:email])
     end
@@ -33,4 +57,11 @@ class PasswordResetsController < ApplicationController
       end
     end
 
+    # トークンが期限切れかどうか確認する
+    def check_expiration
+      if @user.password_reset_expired?
+        flash[:danger] = "Password reset has expired."
+        redirect_to new_password_reset_url
+      end
+    end
 end
