fccc60da0bbaaec46eeeb51c9bb66e77d00cdb8e
oryzae24 <134453358+oryzae24@users.noreply.github.com>
2026-06-04 16:07:23 +0000

    Implement account activation process with controller logic, user model updates, and integration tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -36,9 +36,10 @@ class User < ApplicationRecord
   end
   
   # 渡されたトークンがダイジェストと一致したらtrueを返す
-  def authenticated?(remember_token)
-    return false if remember_digest.nil?
-    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  def authenticated?(attribute, token)
+    digest = send("#{attribute}_digest")
+    return false if digest.nil?
+    BCrypt::Password.new(digest).is_password?(token)
   end
 
   # ユーザーのログイン情報を破棄する
@@ -51,7 +52,17 @@ class User < ApplicationRecord
   def feed
     Micropost.where("user_id = ?", id)
   end
+  
+  # アカウントを有効にする
+  def activate
+    update_attribute(:activated,    true)
+    update_attribute(:activated_at, Time.zone.now)
+  end
 
+  # 有効化用のメールを送信する
+  def send_activation_email
+    UserMailer.account_activation(self).deliver_now
+  end
   private
 
   # メールアドレスをすべて小文字にする
