431c5122293e0dfdeea8388be4651cb3e718eacc
oryzae24 <134453358+oryzae24@users.noreply.github.com>
2026-06-04 12:43:06 +0000

    Add account activation functionality with controller, helper, migration, and tests

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2026_06_03_075922) do
+ActiveRecord::Schema[8.0].define(version: 2026_06_04_124130) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -56,6 +56,9 @@ ActiveRecord::Schema[8.0].define(version: 2026_06_03_075922) do
     t.string "password_digest"
     t.string "remember_digest"
     t.boolean "admin", default: false
+    t.string "activation_digest"
+    t.boolean "activated", default: false
+    t.datetime "activated_at"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
