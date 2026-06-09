b75e0b08a923a0f5af2acefa8ac9e8c05d00593d
oryzae24 <134453358+oryzae24@users.noreply.github.com>
2026-06-09 16:06:17 +0000

    Add user following

--- app/models/user.rb
+++ app/models/user.rb
@@ -84,6 +84,12 @@ class User < ApplicationRecord
     reset_sent_at < 2.hours.ago
   end
 
+  # ユーザーのステータスフィードを返す
+  def feed
+    following_ids = "SELECT followed_id FROM relationships WHERE  follower_id = :user_id"
+    Micropost.where("user_id IN (#{following_ids}) OR user_id = :user_id", user_id: id).includes(:user, image_attachment: :blob)
+  end
+
   # ユーザーをフォローする
   def follow(other_user)
     following << other_user unless self == other_user
